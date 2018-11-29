60bee71d8c44f2f43fe6c5c79b47c3313b6748c6
mariomui <yi.kan.mui@gmail.com>
2018-11-28 16:37:40 -0800

    Implement reducers

--- src/actions/search.js
+++ src/actions/search.js
@@ -4,9 +4,15 @@ import changeVideo from './currentVideo.js';
 import YOUTUBE_API_KEY from '../config/youtube.js';
 
 
-var handleVideoSearch = (q) => ({
-  type: 'query',
-  q
-});
+var handleVideoSearch = (q) => {
+  var options = {
+    q: q,
+    max: 5,
+    key: YOUTUBE_API_KEY
+  };
+  return ( () => {
+    searchYouTube(options, () => {});
+  });
+};
 
 export default handleVideoSearch;
